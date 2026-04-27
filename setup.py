bf1381a4ce14e64bc19594395887762dc99e2dd0
Trishula Software <engineering@trishulasoftware.com>
2026-04-27 16:28:47 -0400

    fix(ci): Root-cause remediation — add setup.py for Cython build, timeout for daemon scripts, mkdir telemetry

---
+++ setup.py
@@ -0,0 +1,8 @@
+﻿from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    name="IsolationSplinter",
+    ext_modules=cythonize("ghost_pdf.py", compiler_directives={'language_level': "3"}),
+    zip_safe=False,
+)
